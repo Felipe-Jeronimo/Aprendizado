1529699c5974ceac9e5952935299dc1e42f107dc
Felipe-Jeronimo <106779558+Felipe-Jeronimo@users.noreply.github.com>
2022-10-21 21:59:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,57 +2,43 @@ function validacao(){
 let usuarioValido = "Felipe";
 let senhaValida = "123456";
 
-    let usuario =document.getElementById("usuario").value;
+    let usuario = document.getElementById("usuario").value;
     
-    let senha = document.getElementById ("senha").value;
-    
+    let senha = document.getElementById("senha").value;
+          
 //usuario vazio
     if (usuario == "" && senha ==""){
         alert ("Não foi possivel logar, usuario e senha vazio");
     }
-    else if (usuario == "" && senha != "senhaValida"){
-        alert ("Não foi possivel logar, porfavor preencha o usuario");
+    else if (usuario == "" && senha == senhaValida){
+        alert ("Não foi possivel logar, por favor preencha o usuario");
     }
-    else if (usuario == "" && senha != "senhaValida"){
-        alert ("Não foi possivel logar, porfavor preencha o usuario");
+    else if (usuario == "" && senha != senhaValida){
+        alert ("Não foi possivel logar, usuario incorrento");          
     }
-/*
+        
 //usuario invalido
-    else if (usuario != "usuarioValido" && senha == ""){
+    else if (usuario != usuarioValido && senha == ""){
+        alert ("Não foi possivel logar, login incorreto" );
+    }
+    else if (usuario != usuarioValido && senha == senhaValida){
         alert ("Não foi possivel logar, login incorreto");
     }
-    else if (usuario != "usuarioValido" && senha == "senhaValida"){
-        alert ("Não foi possivel logar, login incorreto");
-    }
-    else if (usuario != "usuarioValido" && senha != "senhaValida"){
+    else if (usuario != usuarioValido && senha != senhaValida){
         alert ("Não foi possivel logar, login incorreto");
     }
     
-    //usuario valido
-    else if (usuario == "usuarioValido" && senha == ""){
+//usuario valido
+    else if (usuario == usuarioValido && senha == ""){
         alert ("Não foi possivel logar, a senha não foi digitada");
     }
-    else if (usuario == "usuarioValido" && senha != "senhaValida"){
+    else if (usuario == usuarioValido && senha != senhaValida){
         alert ("Não foi possivel logar, senha incorreta");
     }
-    else if (usuario == "usuarioValido" && senha == "senhavalida"){
+    else if(usuario == usuarioValido && senha == senhaValida){
         alert ("Logado com sucesso");
+        window.open("http://127.0.0.1:5500/NovaPagina.html","_self");
+        //document.write("<p></p>");        
+        }
     }
- 
-       
-    *usuario vazio, senha vazia
-    *usuario vazio, senha valida
-    *usuario vazio, senha invalida
     
-    usuario invalido, senha vazia 
-    usuario invalido, senha valida
-    usuario invalido, senha invalida
-    
-    usuario valido, senha vazia
-    usuario valido, senha invalida
-
-    usuario valido, senha valida
-  */  
-
-
-    }
